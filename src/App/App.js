ef40739783cf07af02bee54c443146f9844b431d
koecha <peterkoech1624@gmail.com>
2021-10-14 00:05:00 +0300

    koecha

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Annotorious } from '@recogito/annotorious';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react'
+import { Annotorious } from '@recogito/annotorious'
 import './App.css'
 
-import '@recogito/annotorious/dist/annotorious.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
+// import { initializeApp } from "firebase/app"
+// import { getFirestore as db } from "firebase/firestore"
+
+
+import '@recogito/annotorious/dist/annotorious.min.css'
+import { Container, Row, Col } from 'react-bootstrap'
+
+
 
 
 
@@ -13,138 +18,144 @@ function App() {
 
   const [imgSrc, setImgSrc] = useState(null)
   const imgRef = useRef(null)
-  const [fileName, setFileName] = useState('')
+  // const [fileName, setFileName] = useState('')
   const [anno, setAnno] = useState(null)
 
 
   const handleFileChange = (e) => {
     setImgSrc(URL.createObjectURL(e.target.files[0]))
-    setFileName(e.target.files[0].name)
-    console.log(fileName)
+    
   }
 
+ 
   useEffect(() => {
 
-    let annotorious = null;
+    let annotorious = null
 
-    if (imgRef.current) {
+    if (imgRef.current && imgSrc !== null) {
 
       annotorious = new Annotorious({
         image: imgRef.current,
         widgets: [
           'COMMENT'
         ]
-      });
+      })
 
-      annotorious.on('createAnnotation', annotation => {
-        localStorage.setItem(`${new Date()}`, JSON.stringify(annotation))
-        console.log('created', annotation)
-      });
+      setImgSrc(imgSrc)
+    // JSON.parse(localStorage.getItem(imgSrc))
+    if (JSON.parse(localStorage.getItem(imgSrc)) === null && imgSrc !== null ) localStorage.setItem(imgSrc, JSON.stringify([]))
 
-      annotorious.on('updateAnnotation', (annotation, previous) => {
-        console.log('updated', annotation, previous);
-      });
 
-      annotorious.on('deleteAnnotation', annotation => {
-        console.log('deleted', annotation);
-      });
+
+      annotorious.on('createAnnotation', function (annotation) {
+
+        let storage = JSON.parse(localStorage.getItem(imgSrc))
+        storage.push(annotation)
+        localStorage.setItem(imgSrc, JSON.stringify(storage))
+
+        // JSON.parse(localStorage.getItem(imgRef.current)).push(annotation)
+        console.log('Stored annotation')
+
+      })
+
     }
 
     setAnno(annotorious)
+  
 
-    return () => annotorious.destroy();
+    if (imgSrc !== null) return () => annotorious.destroy()
 
   }, [imgSrc])
 
 
   const saveAnnoImg = () => {
-    alert('Changes Saved ')
+    setImgSrc(null)
+    alert('Changes Saved! Upload new photo!')
   }
 
 
   const remove = () => {
-    setImgSrc(null)
+    anno.clearAnnotations()
   }
 
-  const handleDisplay = (imgValue) => {
+  const handleDisplay = (key) => {
+    let imgValue = key
     setImgSrc(imgValue)
-    anno.getAnnotations()
+    let imgAnnotations = JSON.parse(localStorage.getItem(imgValue))
+
+    console.log(imgAnnotations)
+    anno.setAnnotations(imgAnnotations)
+    
+    
   }
 
+
+  // Object.keys(localStorage).forEach((key) => (
+    
+  //   // console.log(JSON.parse(localStorage.getItem(key)))
+  // ))
 
 
   return (
-    <Router>
-      <Route exact path="/">
-        <Container className="App">
-          <Row>
-            <Col xs={12} md={8}>
 
-              <h1>Annotation Demo.</h1>
+    <Container className="App">
+      <Row>
+        <Col xs={12} md={8}>
 
-              <div className="py-3">
-                <img
-                  ref={imgRef}
-                  src={imgSrc}
-                  className="image"
-                  alt="Upload to Annotate" />
-              </div>
+          <h1>Annotation Demo.</h1>
 
-
-              {imgSrc !== null && (
-                <div>
-                  <button className="A-button">Annotate</button>
-                  <button className="R-button" onClick={remove}>Remove</button>
-                </div>
-              )}
-
-              {imgSrc === null ? (
-                <div>
-                  <input onChange={handleFileChange} type="file" id="actual-btn" hidden />
-                  <label className="label" htmlFor="actual-btn">Upload Photo</label>
+          <div className="py-3">
+            <img
+              ref={imgRef}
+              src={imgSrc}
+              className="image"
+              alt="Upload to Annotate" />
+          </div>
 
 
-                </div>
-              ) : (
-                <div>
-                  <button className="label" onClick={saveAnnoImg} >Save</button>
-                </div>
-              )}
-            </Col>
+          {imgSrc !== null && (
+            <div>
+              <button className="A-button">Annotate</button>
+              <button className="R-button" onClick={remove}>Remove</button>
+            </div>
+          )}
 
-            <Col>
-
-              <h1>My Annotations</h1>
-
-              <div className="d-grid">
-                {localStorage.length === 0 ? (
-                  <p>You have not saved any image with Annotations</p>
-                ) : (
-
-                  Object.keys(localStorage).map((keys, index) => {
-
-                    const values = JSON.parse(localStorage.getItem(keys))
-
-                    const imgValue = values.target.source
+          {imgSrc === null ? (
+            <div>
+              <input onChange={handleFileChange} type="file" id="actual-btn" hidden />
+              <label className="label" htmlFor="actual-btn">Upload Photo</label>
 
 
+            </div>
+          ) : (
+            <div>
+              <button className="label" onClick={saveAnnoImg} >Save</button>
+            </div>
+          )}
+        </Col>
 
-                    return (
-                      <button className="m-1" key={index} onClick={() => handleDisplay(imgValue)} >{`Annotation ${index + 1}`}</button>
-                    )
-                  }
-                  ))}
-              </div>
-              {localStorage.length !== 0 && (
-                <button onClick={() => localStorage.clear()}>Delete All</button>
-              )}
-              
-            </Col>
-          </Row>
-        </Container>
-      </Route>
-    </Router>
-  );
+        <Col>
+
+          <h1>My Annotations</h1>
+
+          <div className="d-grid">
+            {
+              Object.keys(localStorage).map((key, index) => (
+
+                <button className="m-1" key={index} onClick={() => handleDisplay(key)}>{`Annotation ${index + 1}`}</button>
+
+              ))
+            }
+            {/* console.log(key)
+                // console.log(JSON.parse(localStorage.getItem(key))) */}
+
+          </div>
+
+        </Col>
+      </Row>
+    </Container>
+
+  )
 }
 
-export default App;
+export default App
